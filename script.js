ce48279da8fe72b38e4dfbd1f8c032f046e2f168
Jenisha Shrestha <164962901+Jnishha@users.noreply.github.com>
2024-10-11 18:58:20 +0545

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ setInterval(
     function(){
         d = new Date();
         second = d.getSeconds()*6;
-        minute = d.getMinutes();
-        hour = d.getHours();
+        minute = d.getMinutes()*6;
+        hour = d.getHours()*30;
         document.getElementById("sec").style.transform = "rotate("+ second + "deg)";
         document.getElementById("min").style.transform = "rotate("+ minute + "deg)";
         document.getElementById("hr").style.transform = "rotate("+ hour + "deg)";
