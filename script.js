daa898cbed33de3f7ee7979b3c080245fbc1fb62
Jnishhha <jnishhha@gmail.com>
2024-10-15 20:44:46 +0545

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ setInterval(
         second = d.getSeconds()*6;
         minute = d.getMinutes()*6;
         hour = d.getHours()*30 + Math.round(minute / 12);
-        document.getElementById("sec").style.transform = "rotate(" + second + "deg)";
+        // document.getElementById("sec").style.transform = "rotate(" + second + "deg)";
         document.getElementById("min").style.transform = "rotate("+ minute + "deg)";
         document.getElementById("hr").style.transform = "rotate("+ hour + "deg)";
     },1000
